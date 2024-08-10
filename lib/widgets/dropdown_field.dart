d7176df62fbdb326f04d649cdb555f25a48d16ea
Ax0elz <alex.kotliarsky@gmail.com>
2024-08-10 00:13:42 -0600

    adjust dropdown padding

--- lib/widgets/dropdown_field.dart
+++ lib/widgets/dropdown_field.dart
@@ -118,7 +118,7 @@ class _DropDownFieldState<T> extends State<_DropDownField<T>> {
       onTap: widget.onTap,
       child: Container(
         height: widget.height,
-        padding: widget.headerPadding ?? _defaultHeaderPadding,
+        padding: widget.headerPadding ?? _defaultDropdownHeaderPadding,
         decoration: BoxDecoration(
           color: widget.fillColor ??
               Theme.of(context).colorScheme.outline.withOpacity(
